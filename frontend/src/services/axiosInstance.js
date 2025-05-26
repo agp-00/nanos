23f4121fea7d92e24c3c5f338b1bb792c9aa644d
Adrián Gómez Perelló <agp00@iesemilidarder.com>
2025-05-26 21:04:47 +0200

    commit

---
+++ frontend/src/services/axiosInstance.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api',
+  headers: {
+    Accept: 'application/json',
+  }
+});
+
+// Cargar token si existe
+const token = localStorage.getItem('token');
+if (token) {
+  instance.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export default instance;
