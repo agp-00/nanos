faaf8f1e5397589795d741e89555c116e12d94f2
Adrián Gómez Perelló <agp00@iesemilidarder.com>
2025-06-09 22:44:02 +0200

    productos

--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -11,10 +11,37 @@ class ProductSeeder extends Seeder
     public function run()
     {
         Producto::create([
-            'nombre' => 'Oscar',
+            'nombre' => 'Castillo hinchable',
+            'cantidad' => 1,
+            'categoria' => 'atracción',
+            'descripcion' => 'Atracción de castillo hinchable',
+            'imagen' => 'productos/castillohinchable.jpg',
+        ]);
+
+        Producto::create([
+            'nombre' => 'Platos',
+            'cantidad' => 1,
+            'categoria' => 'material',
+            'descripcion' => 'Plato de comida',
+            'imagen' => 'productos/plato.jpg',
+        ]);
+
+        Producto::create([
+            'nombre' => 'Saltador de goma',
+            'cantidad' => 1,
+            'categoria' => 'juguete',
+            'descripcion' => 'Saltimbanquis',
+            'imagen' => 'productos/saltador.webp',
+
+        ]);
+
+        Producto::create([
+            'nombre' => 'Globos',
             'cantidad' => 1,
             'categoria' => 'otro',
-            'descripcion' => 'osquitar uwu follame',
+            'descripcion' => 'globos hinchables',
+            'imagen' => 'productos/globos.jpg',
+
         ]);
     }
 }
