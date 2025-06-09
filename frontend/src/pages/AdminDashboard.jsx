91f006fa142130cb9d5677dfb1144e9a62d81fe7
Adrián Gómez Perelló <agp00@iesemilidarder.com>
2025-06-09 21:44:59 +0200

    commit

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,36 +1,36 @@
 import { useNavigate } from 'react-router-dom';
 
 function AdminDashboard() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <div className="p-8 max-w-xl mx-auto text-center">
-      <h1 className="text-2xl font-bold mb-6">Panel de Administración</h1>
+    return (
+        <div className="p-8 max-w-xl mx-auto text-center">
+            <h1 className="text-2xl font-bold mb-6">Panel de Administración</h1>
 
-      <div className="grid gap-4">
-        <button
-          onClick={() => navigate('/admin/inventario')}
-          className="bg-blue-600 text-white px-6 py-3 rounded hover:bg-blue-700"
-        >
-          Gestionar Inventario
-        </button>
-
-        <button
-          onClick={() => navigate('/admin/reservas')}
-          className="bg-yellow-400 text-white px-6 py-3 rounded hover:bg-yellow-700"
-        >
-          Gestionar Reservas
-        </button>
+            <div className="grid gap-4">
+                <button
+                    onClick={() => navigate('/admin/inventario')}
+                    className="bg-blue-600 text-white px-6 py-3 rounded hover:bg-blue-700"
+                >
+                    Gestionar Inventario
+                </button>
 
                 <button
-          onClick={() => navigate('/admin/semanas')}
-          className="bg-red-600 text-white px-6 py-3 rounded hover:bg-red-700"
-        >
-          Gestionar Semanas
-        </button>
-      </div>
-    </div>
-  );
+                    onClick={() => navigate('/admin/reservas')}
+                    className="bg-yellow-400 text-white px-6 py-3 rounded hover:bg-yellow-700"
+                >
+                    Gestionar Reservas
+                </button>
+
+                <button
+                    onClick={() => navigate('/admin/semanas')}
+                    className="bg-red-600 text-white px-6 py-3 rounded hover:bg-red-700"
+                >
+                    Gestionar Semanas
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default AdminDashboard;
