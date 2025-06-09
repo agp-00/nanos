71e75f4d762eec603a593f20425e6c74581a6859
Adrián Gómez Perelló <agp00@iesemilidarder.com>
2025-06-09 21:58:47 +0200

    commit

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(HorariosSemanalesSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(ProductSeeder::class);
     }
 
 }
